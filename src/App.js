eada84f158deff8165ac70b800685fb7cb615b99
Alexander Ignacz <ignaczalexander@gmail.com>
2020-08-17 18:55:08 +0200

    Refactor to use redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
-import source from "./data/example-data.json";
+import React, { useEffect } from "react";
 import Table from "./components/Table";
-import { addId, removeRow } from "./utils";
+import { connect } from "react-redux";
+import { loadData, removeData } from "./redux/modules/data";
 
-export default function App() {
-  const [data, setData] = useState(addId(source));
-  console.log("initial data", data);
+function App(props) {
+  const { data, loadData, removeData } = props;
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
 
   function handleDelete(id) {
-    const newData = removeRow(data, id);
-    setData(newData);
-    console.log("newData", newData);
+    removeData(id);
   }
 
+  if (data.length === 0) return null;
   return (
     <div style={{ padding: "1em" }}>
       <Table rows={data} onRowDelete={handleDelete} />
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  data: state.data.data,
+});
+
+export default connect(mapStateToProps, {
+  loadData,
+  removeData,
+})(App);
